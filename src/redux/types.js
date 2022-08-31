3969b5b7bf8ac26eb8c96ba0374318a63961a93a
Laceinstein <laceinstein@gmail.com>
2022-08-31 14:35:02 +0200

    ajout des dossier et des dependances redux et react-redux

---
+++ src/redux/types.js
@@ -0,0 +1,6 @@
+export const ADD_USER = 'ADD_USER'
+export const ADD_ACCOUNT = 'ADD_ACCOUNT'
+export const ADD_TRANSACTION = 'ADD_TRANSACTION'
+export const GET_USERS = 'GET_USERS'
+export const GET_ACCOUNTS = 'GET_ACCOUNTS'
+export const GET_TRANSACTIONS = 'GET_TRANSACTIONS'
